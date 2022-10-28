0d6445fe4cc768078ade010631c7d29272244a29
Neha Sahu <91769030+Neha2580@users.noreply.github.com>
2022-10-28 19:23:05 +0530

    Create repeatedele.cpp

---
+++ repeatedele.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printTwoRepeatNumber(int arr[], int size)
+{
+	int i, j;
+	cout << "Repeating elements are ";
+	for (i = 0; i < size; i++) {
+		for (j = i + 1; j < size; j++) {
+			if (arr[i] == arr[j]) {
+				cout << arr[i] << " ";
+				break;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int arr[] = { 4, 2, 4, 5, 2, 3, 1 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	printTwoRepeatNumber(arr, arr_size);
+	return 0;
+}
